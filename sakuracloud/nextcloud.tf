b0fab1a394ee74ee011cbf48e9b328ccb5abef77
Kazuto Kusama <jacopen@gmail.com>
2022-11-03 21:00:40 +0900

    Remove old nextcloud

--- sakuracloud/nextcloud.tf
+++ sakuracloud/nextcloud.tf
@@ -10,69 +10,6 @@ data "sakuracloud_archive" "ubuntu" {
   }
 }
 
-resource "sakuracloud_disk" "nextcloud_boot" {
-  name              = "nextcloud-boot"
-  source_archive_id = data.sakuracloud_archive.ubuntu.id
-  plan              = "ssd"
-  connector         = "virtio"
-  size              = 20
-
-  lifecycle {
-    ignore_changes = [
-      source_archive_id,
-    ]
-  }
-}
-
-resource "sakuracloud_disk" "nextcloud_data" {
-  name      = "nextcloud-data"
-  plan      = "ssd"
-  connector = "virtio"
-  size      = 2048
-
-  lifecycle {
-    ignore_changes = [
-      source_archive_id,
-    ]
-  }
-}
-
-resource "sakuracloud_disk" "nextcloud_backup" {
-  name      = "nextcloud-backup"
-  plan      = "hdd"
-  connector = "virtio"
-  size      = 2048
-
-  lifecycle {
-    ignore_changes = [
-      source_archive_id,
-    ]
-  }
-}
-
-resource "sakuracloud_server" "nextcloud" {
-  name = "nextcloud"
-  disks = [
-    sakuracloud_disk.nextcloud_boot.id,
-    sakuracloud_disk.nextcloud_data.id,
-    sakuracloud_disk.nextcloud_backup.id
-  ]
-  core        = 2
-  memory      = 4
-  description = "Nextcloud server"
-  tags        = ["app=nextcloud", "stage=production", "starred"]
-
-  network_interface {
-    upstream = "shared"
-  }
-
-  disk_edit_parameter {
-    hostname    = "nextcloud"
-    password    = random_password.password.result
-    ssh_key_ids = [for l in sakuracloud_ssh_key.key : l.id]
-  }
-}
-
 resource "sakuracloud_disk" "nextcloud_boot2" {
   name              = "nextcloud-boot2"
   source_archive_id = data.sakuracloud_archive.ubuntu.id
