949ef11dcb4329638dab8a5ee2b8a46a5d64e540
Kazuto Kusama <jacopen@gmail.com>
2022-11-27 17:45:31 +0900

    Add tailscale vm

---
+++ sakuracloud/servers.tf
@@ -0,0 +1,90 @@
+resource "sakuracloud_disk" "uploader_boot" {
+  name              = "uploader-boot"
+  source_archive_id = data.sakuracloud_archive.ubuntu.id
+  plan              = "ssd"
+  connector         = "virtio"
+  size              = 100
+
+  lifecycle {
+    ignore_changes = [
+      source_archive_id,
+    ]
+  }
+}
+
+resource "sakuracloud_server" "uploader" {
+  name = "uploader"
+  disks = [
+    sakuracloud_disk.uploader_boot.id,
+  ]
+  core        = 2
+  memory      = 4
+  description = "Nextcloud Sandbox"
+  tags        = ["app=uploader", "stage=production", "starred"]
+
+  network_interface {
+    upstream         = "shared"
+    packet_filter_id = sakuracloud_packet_filter.nextcloud.id
+  }
+
+  network_interface {
+    upstream = data.sakuracloud_switch.switcher.id
+  }
+
+  user_data = templatefile("./template/cloud-init.yaml", {
+    vm_password = random_password.password.result,
+    hostname    = "uploader"
+  })
+
+  lifecycle {
+    ignore_changes = [
+      user_data,
+    ]
+  }
+}
+
+resource "sakuracloud_disk" "tailscale_boot" {
+  name              = "tailscale"
+  source_archive_id = data.sakuracloud_archive.ubuntu.id
+  plan              = "ssd"
+  connector         = "virtio"
+  size              = 20
+
+  lifecycle {
+    ignore_changes = [
+      source_archive_id,
+    ]
+  }
+}
+
+resource "sakuracloud_server" "tailscale" {
+  name = "tailscale"
+  disks = [
+    sakuracloud_disk.tailscale_boot.id,
+  ]
+  core        = 1
+  memory      = 2
+  description = "Nextcloud Sandbox"
+  tags        = ["app=tailscale", "stage=production", "starred"]
+
+  network_interface {
+    upstream         = "shared"
+    packet_filter_id = sakuracloud_packet_filter.nextcloud.id
+  }
+
+  network_interface {
+    upstream = data.sakuracloud_switch.switcher.id
+  }
+
+  user_data = templatefile("./template/nextcloud.yaml", {
+    vm_password = var.vm_password,
+    hostname    = "nextcloud",
+    secondary_ip = "192.168.71.249"
+  })
+
+  lifecycle {
+    ignore_changes = [
+      user_data,
+    ]
+  }
+}
