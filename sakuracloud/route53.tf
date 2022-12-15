5549a1d8551fa7579c4f2a9388e0ffd77524d07f
Kazuto Kusama <jacopen@gmail.com>
2022-12-15 21:02:41 +0900

    Modify route53

--- sakuracloud/route53.tf
+++ sakuracloud/route53.tf
@@ -3,39 +3,12 @@ data "aws_route53_zone" "cloudnativedays" {
   private_zone = false
 }
 
-resource "aws_route53_record" "nextcloud2" {
-  zone_id = data.aws_route53_zone.cloudnativedays.zone_id
-  name    = "nextcloud2.cloudnativedays.jp"
-  type    = "A"
-  ttl     = "300"
-  records = [sakuracloud_server.nextcloud2.ip_address]
-}
-
 resource "aws_route53_record" "sentry" {
   zone_id = data.aws_route53_zone.cloudnativedays.zone_id
   name    = "sentry.cloudnativedays.jp"
   type    = "A"
   ttl     = "300"
   records = [sakuracloud_server.sentry.ip_address]
-}
-
-resource "aws_route53_record" "uploader" {
-  zone_id = data.aws_route53_zone.cloudnativedays.zone_id
-  name    = "uploader.cloudnativedays.jp"
-  type    = "A"
-  ttl     = "300"
-  records = [sakuracloud_server.nextcloud.ip_address]
-  depends_on = [
-    sakuracloud_proxylb.uploader
-  ]
-}
-
-resource "aws_route53_record" "uploader_internal" {
-  zone_id = data.aws_route53_zone.cloudnativedays.zone_id
-  name    = "uploader-internal.cloudnativedays.jp"
-  type    = "A"
-  ttl     = "300"
-  records = [sakuracloud_server.uploader.ip_address]
 }
 
 resource "aws_route53_record" "elk" {
@@ -46,10 +19,10 @@ resource "aws_route53_record" "elk" {
   records = [sakuracloud_server.elk.ip_address]
 }
 
-resource "aws_route53_record" "nc-sandbox" {
+resource "aws_route53_record" "nc_sandbox_internal" {
   zone_id = data.aws_route53_zone.cloudnativedays.zone_id
-  name    = "nc-sandbox.cloudnativedays.jp"
+  name    = "nc-sandbox-internal.cloudnativedays.jp"
   type    = "A"
   ttl     = "300"
-  records = [sakuracloud_server.nc_sandbox.ip_address]
+  records = ["192.168.71.112"]
 }
