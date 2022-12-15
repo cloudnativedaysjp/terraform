62f0687f14800d5cd78f8b59185635e8a3ded0cd
Kazuto Kusama <jacopen@gmail.com>
2022-12-15 21:11:59 +0900

    Change DNS

--- sakuracloud/route53.tf
+++ sakuracloud/route53.tf
@@ -19,10 +19,26 @@ resource "aws_route53_record" "elk" {
   records = [sakuracloud_server.elk.ip_address]
 }
 
+resource "aws_route53_record" "nc_sandbox" {
+  zone_id = data.aws_route53_zone.cloudnativedays.zone_id
+  name    = "nc-sandbox.cloudnativedays.jp"
+  type    = "A"
+  ttl     = "300"
+  records = [sakuracloud_server.nc_sandbox.ip_address]
+}
+
 resource "aws_route53_record" "nc_sandbox_internal" {
   zone_id = data.aws_route53_zone.cloudnativedays.zone_id
   name    = "nc-sandbox-internal.cloudnativedays.jp"
   type    = "A"
   ttl     = "300"
   records = ["192.168.71.112"]
+}
+
+resource "aws_route53_record" "nextcloud_internal" {
+  zone_id = data.aws_route53_zone.cloudnativedays.zone_id
+  name    = "nextcloud-internal.cloudnativedays.jp"
+  type    = "A"
+  ttl     = "300"
+  records = ["192.168.71.111"]
 }
