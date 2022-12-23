19cc5fe05bc2ac654582228bc98f010c587181b0
Kazuto Kusama <jacopen@gmail.com>
2022-12-23 18:29:34 +0900

    Change uptime robot

--- uptime_robot/monitors.tf
+++ uptime_robot/monitors.tf
@@ -4,15 +4,15 @@ resource "uptimerobot_monitor" "dreamkast" {
   type          = "http"
   url           = "https://event.cloudnativedays.jp"
 
-  alert_contact {
-    id = uptimerobot_alert_contact.slack.id
-  }
+  # alert_contact {
+  #   id = uptimerobot_alert_contact.slack.id
+  # }
 
-  alert_contact {
-    id = data.uptimerobot_alert_contact.default_alert_contact.id
-  }
+  # alert_contact {
+  #   id = data.uptimerobot_alert_contact.default_alert_contact.id
+  # }
 
-  interval = 60
+  interval = 300
 
   lifecycle {
     ignore_changes = [
@@ -31,15 +31,15 @@ resource "uptimerobot_monitor" "website" {
   type          = "http"
   url           = "https://cloudnativedays.jp"
 
-  alert_contact {
-    id = uptimerobot_alert_contact.slack.id
-  }
+  # alert_contact {
+  #   id = uptimerobot_alert_contact.slack.id
+  # }
 
-  alert_contact {
-    id = data.uptimerobot_alert_contact.default_alert_contact.id
-  }
+  # alert_contact {
+  #   id = data.uptimerobot_alert_contact.default_alert_contact.id
+  # }
 
-  interval = 60
+  interval = 300
 
   lifecycle {
     ignore_changes = [
@@ -58,15 +58,15 @@ resource "uptimerobot_monitor" "grafana" {
   type          = "http"
   url           = "https://grafana.cloudnativedays.jp"
 
-  alert_contact {
-    id = uptimerobot_alert_contact.slack.id
-  }
+  # alert_contact {
+  #   id = uptimerobot_alert_contact.slack.id
+  # }
 
-  alert_contact {
-    id = data.uptimerobot_alert_contact.default_alert_contact.id
-  }
+  # alert_contact {
+  #   id = data.uptimerobot_alert_contact.default_alert_contact.id
+  # }
 
-  interval = 60
+  interval = 300
 
   lifecycle {
     ignore_changes = [
@@ -80,15 +80,15 @@ resource "uptimerobot_monitor" "nextcloud" {
   type          = "http"
   url           = "https://uploader.cloudnativedays.jp"
 
-  alert_contact {
-    id = uptimerobot_alert_contact.slack.id
-  }
+  # alert_contact {
+  #   id = uptimerobot_alert_contact.slack.id
+  # }
 
-  alert_contact {
-    id = data.uptimerobot_alert_contact.default_alert_contact.id
-  }
+  # alert_contact {
+  #   id = data.uptimerobot_alert_contact.default_alert_contact.id
+  # }
 
-  interval = 60
+  interval = 300
 
   lifecycle {
     ignore_changes = [
@@ -105,15 +105,15 @@ resource "uptimerobot_monitor" "prometheus" {
   http_username = "prometheus"
   http_password = var.prometheus_password
 
-  alert_contact {
-    id = uptimerobot_alert_contact.slack.id
-  }
+  # alert_contact {
+  #   id = uptimerobot_alert_contact.slack.id
+  # }
 
-  alert_contact {
-    id = data.uptimerobot_alert_contact.default_alert_contact.id
-  }
+  # alert_contact {
+  #   id = data.uptimerobot_alert_contact.default_alert_contact.id
+  # }
 
-  interval = 60
+  interval = 300
 
   lifecycle {
     ignore_changes = [
