7b056cbd21d65ee7ef0953e746cbbbd1ba4b5d9f
Kazuto Kusama <jacopen@gmail.com>
2022-12-23 17:43:50 +0900

    Add simple monitoring

---
+++ sakuracloud/monitoring.tf
@@ -0,0 +1,107 @@
+resource "sakuracloud_simple_monitor" "uploader" {
+  target = "uploader.cloudnativedays.jp"
+
+  delay_loop = 60
+  timeout    = 10
+
+  max_check_attempts = 3
+  retry_interval     = 10
+
+  health_check {
+    protocol        = "https"
+    port            = 443
+    path            = "/"
+    status          = "302"
+    host_header     = "uploader.cloudnativedays.jp"
+    sni             = false
+    http2           = false
+  }
+
+  description = "Monitoring for NextCloud"
+
+  notify_interval = 0
+  notify_email_enabled = false
+  notify_slack_enabled = true
+  notify_slack_webhook = var.slack_webhook_url
+}
+
+resource "sakuracloud_simple_monitor" "dreamkast" {
+  target = "event.cloudnativedays.jp"
+
+  delay_loop = 60
+  timeout    = 10
+
+  max_check_attempts = 3
+  retry_interval     = 10
+
+  health_check {
+    protocol        = "https"
+    port            = 443
+    path            = "/"
+    status          = "200"
+    host_header     = "event.cloudnativedays.jp"
+    sni             = false
+    http2           = false
+  }
+
+  description = "Monitoring for Dreamkast"
+
+  notify_interval = 0
+  notify_email_enabled = false
+  notify_slack_enabled = true
+  notify_slack_webhook = var.slack_webhook_url
+}
+
+resource "sakuracloud_simple_monitor" "grafana" {
+  target = "grafana.cloudnativedays.jp"
+
+  delay_loop = 60
+  timeout    = 10
+
+  max_check_attempts = 3
+  retry_interval     = 10
+
+  health_check {
+    protocol        = "https"
+    port            = 443
+    path            = "/"
+    status          = "200"
+    host_header     = "grafana.cloudnativedays.jp"
+    sni             = false
+    http2           = false
+  }
+
+  description = "Monitoring for Grafana"
+
+  notify_interval = 0
+  notify_email_enabled = false
+  notify_slack_enabled = true
+  notify_slack_webhook = var.slack_webhook_url
+}
+
+resource "sakuracloud_simple_monitor" "prometheus" {
+  target = "prometheus.cloudnativedays.jp"
+
+  delay_loop = 60
+  timeout    = 10
+
+  max_check_attempts = 3
+  retry_interval     = 10
+
+  health_check {
+    protocol        = "https"
+    port            = 443
+    path            = "/"
+    status          = "200"
+    host_header     = "prometheus.cloudnativedays.jp"
+    sni             = false
+    http2           = false
+  }
+
+  description = "Monitoring for Prometheus"
+
+  notify_interval = 0
+  notify_email_enabled = false
+  notify_slack_enabled = true
+  notify_slack_webhook = var.slack_webhook_url
+}
--- sakuracloud/variables.tf
+++ sakuracloud/variables.tf
@@ -1,1 +1,5 @@
 variable "vm_password" {}
+variable "slack_webhook_url" {
+  type        = string
+  description = "Webhook url for slack"
+}
