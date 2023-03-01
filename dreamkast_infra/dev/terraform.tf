54ebb9a803ca8a35edcfd52366277735b0d13e8f
kmnky <cubethe27@gmail.com>
2023-03-01 20:21:57 +0900

    terraform cloudを指定

--- dreamkast_infra/dev/terraform.tf
+++ dreamkast_infra/dev/terraform.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 4.0"
     }
   }
+  cloud {
+    organization = "cloudnativedaysjp"
+
+    workspaces {
+      name = "dreamkast_infra_dev"
+    }
+  }
 }
 
 provider "aws" {
