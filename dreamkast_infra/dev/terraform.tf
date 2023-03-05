4670c0a037c1979129ff1db0da0c801d2dcbc0fa
kmnky <cubethe27@gmail.com>
2023-03-05 21:44:47 +0900

    set kubernetes provider

--- dreamkast_infra/dev/terraform.tf
+++ dreamkast_infra/dev/terraform.tf
@@ -17,3 +17,13 @@ terraform {
 provider "aws" {
   region = "us-west-2"
 }
+
+data "aws_eks_cluster_auth" "default" {
+  name = var.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.default.token
+}
