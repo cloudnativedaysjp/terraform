ed9983401c5cc714a72d27d60b247a3155bd215b
kmnky <cubethe27@gmail.com>
2023-03-05 21:45:42 +0900

    コメント追加

--- dreamkast_infra/dev/terraform.tf
+++ dreamkast_infra/dev/terraform.tf
@@ -18,6 +18,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
+# https://github.com/terraform-aws-modules/terraform-aws-eks/issues/2009
 data "aws_eks_cluster_auth" "default" {
   name = var.cluster_name
 }
