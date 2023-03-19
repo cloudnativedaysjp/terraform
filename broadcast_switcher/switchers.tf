49a657e4429174dbc6dd4d272251b61746b07fee
Kazuto Kusama <jacopen@gmail.com>
2023-03-19 17:43:53 +0900

    Fix size

--- broadcast_switcher/switchers.tf
+++ broadcast_switcher/switchers.tf
@@ -48,7 +48,7 @@ resource "sakuracloud_disk" "switcher_data" {
   name              = "${each.value.hostname}-data"
   plan              = "ssd"
   connector         = "virtio"
-  size              = 200
+  size              = 512
 
   lifecycle {
     ignore_changes = [
