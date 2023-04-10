df6cb41725f2f7ff5ea4986704f33883f9626528
kmnky <cubethe27@gmail.com>
2023-04-11 08:32:19 +0900

    cluster idの指定を削除

--- dreamkast_infra/dev/eks.tf
+++ dreamkast_infra/dev/eks.tf
@@ -288,7 +288,6 @@ module "cluster_autoscaler_irsa" {
 
   role_name                        = "${var.prj_prefix}-cluster-autoscaler-irsa"
   attach_cluster_autoscaler_policy = true
-  cluster_autoscaler_cluster_ids   = [module.eks.cluster_id]
 
   oidc_providers = {
     main = {
