9eb0edfb4d5dc01b9fd91a214c5709ab0ece67e4
kmnky <cubethe27@gmail.com>
2023-04-25 20:50:10 +0900

    DNSレコードの作成

---
+++ dreamkast_infra/dev/route53.tf
@@ -0,0 +1,18 @@
+data "aws_lb" "lb" {
+  tags = {
+    "elbv2.k8s.aws/cluster"    = var.cluster_name
+    "service.k8s.aws/resource" = "LoadBalancer"
+    "service.k8s.aws/stack"    = "projectcontour/envoy"
+  }
+}
+
+data "aws_route53_zone" "lb_zone" {
+  name = "cloudnativedays.jp"
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = data.aws_route53_zone.lb_zone.zone_id
+  name    = "*.dev.cloudnativedays.jp	"
+  type    = "A"
+  records = [data.aws_lb.lb.dns_name]
+}
