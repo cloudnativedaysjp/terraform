3a92165da8f5525e819c8c32669b8ece85208862
kmnky <cubethe27@gmail.com>
2023-04-25 21:19:40 +0900

    エイリアスに変更

--- dreamkast_infra/dev/route53.tf
+++ dreamkast_infra/dev/route53.tf
@@ -14,6 +14,9 @@ resource "aws_route53_record" "wildcard_dev_cloudnativedays_jp" {
   zone_id = data.aws_route53_zone.cloudnativedays_jp.zone_id
   name    = "*.dev.cloudnativedays.jp"
   type    = "A"
-  records = [data.aws_lb.lb.dns_name]
-  ttl     = 300
+  alias {
+    name                   = data.aws_lb.lb.dns_name
+    zone_id                = data.aws_lb.lb.zone_id
+    evaluate_target_health = true
+  }
 }
