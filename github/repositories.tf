647389b46b5b09087a73151e490b021fb0548aa8
Kazuto Kusama <jacopen@gmail.com>
2023-05-30 23:30:06 +0900

    Update github

--- github/repositories.tf
+++ github/repositories.tf
@@ -12,6 +12,10 @@ resource "github_repository" "o11y2022" {
   allow_merge_commit   = false
   allow_rebase_merge   = false
   allow_squash_merge   = false
+  merge_commit_message        = ""
+  merge_commit_title          = ""
+  squash_merge_commit_message = ""
+  squash_merge_commit_title   = ""
 }
 
 resource "github_repository" "cndt2021" {
@@ -21,6 +25,26 @@ resource "github_repository" "cndt2021" {
   visibility           = "private"
   has_issues           = true
   has_projects         = true
+  has_wiki             = false
+  auto_init            = false
+  archive_on_destroy   = true
+  vulnerability_alerts = false
+  allow_merge_commit   = false
+  allow_rebase_merge   = false
+  allow_squash_merge   = false
+  merge_commit_message        = ""
+  merge_commit_title          = ""
+  squash_merge_commit_message = ""
+  squash_merge_commit_title   = ""
+}
+
+resource "github_repository" "website" {
+  name        = "website"
+  description = "Website for CloudNative Days a.k.a Dreamkast-Archives"
+
+  visibility           = "public"
+  has_issues           = true
+  has_projects         = true
   has_wiki             = true
   auto_init            = false
   archive_on_destroy   = true
@@ -28,4 +52,8 @@ resource "github_repository" "cndt2021" {
   allow_merge_commit   = false
   allow_rebase_merge   = false
   allow_squash_merge   = false
+  merge_commit_message        = ""
+  merge_commit_title          = ""
+  squash_merge_commit_message = ""
+  squash_merge_commit_title   = ""
 }
--- github/teams.tf
+++ github/teams.tf
@@ -17,3 +17,28 @@ resource "github_team" "dreamkast_admin" {
   name = "dreamkast_admin"
   privacy = "closed"
 }
+
+resource "github_team" "creators" {
+  name = "creators"
+  privacy = "closed"
+}
+
+resource "github_team" "observability" {
+  name = "observability"
+  privacy = "closed"
+}
+
+resource "github_team" "promotion" {
+  name = "promotion"
+  privacy = "closed"
+}
+
+resource "github_team" "cicd2023" {
+  name = "cicd2023"
+  privacy = "closed"
+}
+
+resource "github_team" "cndf2023" {
+  name = "cicd2023"
+  privacy = "closed"
+}
