a61d3c185095b89eb057b89c74e66b6d1ba071e0
Kazuto Kusama <jacopen@gmail.com>
2023-05-30 23:50:09 +0900

    Fix

--- github/repositories.tf
+++ github/repositories.tf
@@ -49,7 +49,7 @@ resource "github_repository" "website" {
   auto_init            = false
   archive_on_destroy   = true
   vulnerability_alerts = false
-  allow_merge_commit   = false
+  allow_merge_commit   = true
   allow_rebase_merge   = false
   allow_squash_merge   = false
   merge_commit_message        = ""
