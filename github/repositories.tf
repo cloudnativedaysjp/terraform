6821f070686bf2239c2371cf8a22bc8940cedebc
Kazuto Kusama <jacopen@gmail.com>
2023-05-31 00:17:37 +0900

    もうわけわからん

--- github/repositories.tf
+++ github/repositories.tf
@@ -3,19 +3,19 @@ resource "github_repository" "o11y2022" {
   description = "observability2021に関わるあれこれを管理していくリポジトリ"
 
   visibility           = "private"
-  has_issues           = true
-  has_projects         = false
-  has_wiki             = false
-  auto_init            = true
-  archive_on_destroy   = true
-  vulnerability_alerts = false
-  allow_merge_commit   = true
-  allow_rebase_merge   = true
-  allow_squash_merge   = true
-  merge_commit_message        = ""
-  merge_commit_title          = ""
-  squash_merge_commit_message = ""
-  squash_merge_commit_title   = ""
+  # has_issues           = true
+  # has_projects         = false
+  # has_wiki             = false
+  # auto_init            = true
+  # archive_on_destroy   = true
+  # vulnerability_alerts = false
+  # allow_merge_commit   = true
+  # allow_rebase_merge   = true
+  # allow_squash_merge   = true
+  # merge_commit_message        = ""
+  # merge_commit_title          = ""
+  # squash_merge_commit_message = ""
+  # squash_merge_commit_title   = ""
 }
 
 resource "github_repository" "cndt2021" {
@@ -23,19 +23,19 @@ resource "github_repository" "cndt2021" {
   description = "CNDT2021に関わるあれこれを管理していくリポジトリ"
 
   visibility           = "private"
-  has_issues           = true
-  has_projects         = true
-  has_wiki             = false
-  auto_init            = false
-  archive_on_destroy   = true
-  vulnerability_alerts = false
-  allow_merge_commit   = true
-  allow_rebase_merge   = true
-  allow_squash_merge   = true
-  merge_commit_message        = ""
-  merge_commit_title          = ""
-  squash_merge_commit_message = ""
-  squash_merge_commit_title   = ""
+  # has_issues           = true
+  # has_projects         = true
+  # has_wiki             = false
+  # auto_init            = false
+  # archive_on_destroy   = true
+  # vulnerability_alerts = false
+  # allow_merge_commit   = true
+  # allow_rebase_merge   = true
+  # allow_squash_merge   = true
+  # merge_commit_message        = ""
+  # merge_commit_title          = ""
+  # squash_merge_commit_message = ""
+  # squash_merge_commit_title   = ""
 }
 
 resource "github_repository" "website" {
@@ -43,19 +43,19 @@ resource "github_repository" "website" {
   description = "Website for CloudNative Days a.k.a Dreamkast-Archives"
 
   visibility           = "public"
-  has_issues           = true
-  has_projects         = false
-  has_wiki             = false
-  auto_init            = false
-  archive_on_destroy   = true
-  vulnerability_alerts = false
-  allow_merge_commit   = true
-  allow_rebase_merge   = true
-  allow_squash_merge   = true
-  merge_commit_message        = ""
-  merge_commit_title          = ""
-  squash_merge_commit_message = ""
-  squash_merge_commit_title   = ""
+  # has_issues           = true
+  # has_projects         = false
+  # has_wiki             = false
+  # auto_init            = false
+  # archive_on_destroy   = true
+  # vulnerability_alerts = false
+  # allow_merge_commit   = true
+  # allow_rebase_merge   = true
+  # allow_squash_merge   = true
+  # merge_commit_message        = ""
+  # merge_commit_title          = ""
+  # squash_merge_commit_message = ""
+  # squash_merge_commit_title   = ""
 }
 
 resource "github_actions_secret" "distribution_id" {
