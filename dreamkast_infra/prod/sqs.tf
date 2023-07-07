4e8f54435ca68a968802854f7fb653c8b9b9cf52
shota-kitazawa <shota.kitazawa@linecorp.com>
2023-07-07 18:59:03 +0900

    create terraform files for dreamkast-prod

---
+++ dreamkast_infra/prod/sqs.tf
@@ -0,0 +1,15 @@
+resource "aws_sqs_queue" "mail_queue" {
+  name                        = "${var.prj_prefix}-mail-queue.fifo"
+  fifo_queue                  = true
+  content_based_deduplication = false
+  delay_seconds               = 0
+  message_retention_seconds   = 60
+}
+
+resource "aws_sqs_queue" "fifo_queue" {
+  name                        = "${var.prj_prefix}-fifo-queue.fifo"
+  fifo_queue                  = true
+  content_based_deduplication = false
+  delay_seconds               = 0
+  message_retention_seconds   = 60
+}
