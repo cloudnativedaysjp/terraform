264611124665d029c7214b3de0f29e67f573918a
Kazuto Kusama <jacopen@gmail.com>
2023-09-06 22:06:31 +0900

    Add hands-on Vm

---
+++ handson/main.tf
@@ -0,0 +1,153 @@
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "cloudnativedaysjp"
+    workspaces {
+      name = "handson"
+    }
+  }
+  required_providers {
+    sakuracloud = {
+      source  = "sacloud/sakuracloud"
+      version = "2.24.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+provider "sakuracloud" {
+}
+provider "github" {}
+
+provider "aws" {
+}
+
+data "sakuracloud_archive" "ubuntu" {
+  filter {
+    id = "113501244046"
+  }
+}
+
+data "aws_route53_zone" "cloudnativedays" {
+  name         = "cloudnativedays.jp."
+  private_zone = false
+}
+
+resource "sakuracloud_disk" "handson_dev01_boot" {
+  name              = "tailscale"
+  source_archive_id = data.sakuracloud_archive.ubuntu.id
+  plan              = "ssd"
+  connector         = "virtio"
+  size              = 20
+
+  lifecycle {
+    ignore_changes = [
+      source_archive_id,
+    ]
+  }
+}
+
+resource "sakuracloud_server" "handson_dev01" {
+  name = "handson-dev01"
+  disks = [
+    sakuracloud_disk.handson_dev01_boot.id,
+  ]
+  core        = 20
+  memory      = 64
+  description = "Hands-on Machine 1"
+  tags        = ["app=handson", "stage=production", "starred"]
+
+  network_interface {
+    upstream         = "shared"
+    packet_filter_id = sakuracloud_packet_filter.handson.id
+  }
+
+  user_data = templatefile("./template/handson-cloud-init.yaml", {
+    vm_password = var.vm_password,
+    hostname    = "handson-dev01",
+  })
+
+  lifecycle {
+    ignore_changes = [
+      user_data,
+    ]
+  }
+}
+
+resource "aws_route53_record" "handson_dev01" {
+  zone_id  = data.aws_route53_zone.cloudnativedays.zone_id
+  name     = "handson-dev01.cloudnativedays.jp"
+  type     = "A"
+  ttl      = "300"
+  records  = [sakuracloud_server.handson_dev01.ip_address]
+}
+
+resource "sakuracloud_packet_filter" "handson" {
+  name        = "handson"
+  description = "Packet filtering rules for handson VMs"
+}
+
+resource "sakuracloud_packet_filter_rules" "handson_rules" {
+  packet_filter_id = sakuracloud_packet_filter.handson.id
+
+  expression {
+    protocol         = "tcp"
+    destination_port = "22"
+  }
+
+  expression {
+    protocol         = "tcp"
+    destination_port = "80"
+  }
+
+  expression {
+    protocol         = "tcp"
+    destination_port = "443"
+  }
+
+  expression {
+    protocol         = "udp"
+    destination_port = "68"
+  }
+
+  expression {
+    protocol = "icmp"
+  }
+
+  expression {
+    protocol = "fragment"
+  }
+
+  expression {
+    protocol    = "udp"
+    source_port = "123"
+  }
+
+  expression {
+    protocol         = "tcp"
+    destination_port = "32768-61000"
+  }
+
+  expression {
+    protocol         = "udp"
+    destination_port = "32768-61000"
+  }
+  expression {
+    protocol    = "ip"
+    allow       = false
+    description = "Deny ALL"
+  }
+}
+
+resource "sakuracloud_internet" "global" {
+  name = "global"
+
+  netmask     = 28
+  band_width  = 250
+  enable_ipv6 = false
+
+  description = "global"
+  tags        = ["global", "production"]
+}
