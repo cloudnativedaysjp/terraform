50c95e05ca383b74698d8e49206cf263b15c987a
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2023-12-03 12:10:25 +0900

    MediaPackageV2用のキャッシュポリシーを追加

---
+++ dreamkast_infra/prod/cloudfront.tf
@@ -0,0 +1,28 @@
+resource "aws_cloudfront_cache_policy" "for_mediapackage_v2" {
+  name        = "MediaPackageV2_prd"
+  comment     = ""
+  default_ttl = 86400
+  max_ttl     = 31536000
+  min_ttl     = 0
+  parameters_in_cache_key_and_forwarded_to_origin {
+    enable_accept_encoding_gzip   = false
+    enable_accept_encoding_brotli = false
+    headers_config {
+      header_behavior = "whitelist"
+      headers {
+        items = [
+          "Origin",
+          "Access-Control-Request-Method",
+          "Access-Control-Allow-Origin",
+          "Access-Control-Request-Header"
+        ]
+      }
+    }
+    cookies_config {
+      cookie_behavior = "all"
+    }
+    query_strings_config {
+      query_string_behavior = "all"
+    }
+  }
+}
