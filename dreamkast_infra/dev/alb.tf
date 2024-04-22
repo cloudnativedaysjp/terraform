5def88000e9139e3a61ff302d92dad85562cfefe
ShotaKitazawa <skitazawa1121@gmail.com>
2024-04-23 00:46:00 +0900

    [dev] update to migrate from EKS to ECS

---
+++ dreamkast_infra/dev/alb.tf
@@ -0,0 +1,67 @@
+resource "aws_lb" "alb" {
+  name               = var.prj_prefix
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = module.vpc.public_subnets
+
+  security_groups = [
+    aws_security_group.alb.id
+  ]
+
+  enable_deletion_protection = true
+
+  #tags = {
+  #  Environment = "${var.prj_prefix}"
+  #}
+}
+
+resource "aws_security_group" "alb" {
+  name   = "${var.prj_prefix}-alb"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    description = "allow HTTPS"
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "allow all"
+    protocol    = "all"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #tags = {
+  #  Environment = "${var.prj_prefix}"
+  #}
+}
+
+resource "aws_lb_listener" "alb" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = aws_acm_certificate.wildcard_dev_cloudnativedays_jp.arn
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      status_code  = "503"
+      message_body = "no appropriately backends"
+    }
+  }
+
+  depends_on = [
+    aws_acm_certificate_validation.wildcard_dev_cloudnativedays_jp
+  ]
+
+  #tags = {
+  #  Environment = "${var.prj_prefix}"
+  #}
+}
--- dreamkast_infra/dev/vpc.tf
+++ dreamkast_infra/dev/vpc.tf
@@ -1,7 +1,9 @@
 data "aws_availability_zones" "available" {}
+data "aws_region" "current" {}
 
 locals {
-  azs = slice(data.aws_availability_zones.available.names, 0, 3)
+  vpc_cidr = "10.110.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 }
 
 
@@ -10,17 +12,18 @@ module "vpc" {
   version = "3.19.0"
 
   name = "${var.prj_prefix}-vpc"
-  cidr = var.vpc_cidr
+  cidr = local.vpc_cidr
 
   azs             = local.azs
-  private_subnets = [for k, _ in local.azs : cidrsubnet(var.vpc_cidr, 4, k)]
-  public_subnets  = [for k, _ in local.azs : cidrsubnet(var.vpc_cidr, 4, k + length(local.azs))]
-  intra_subnets   = [for k, _ in local.azs : cidrsubnet(var.vpc_cidr, 4, k + length(local.azs) * 2)]
+  private_subnets = [for k, _ in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
+  public_subnets  = [for k, _ in local.azs : cidrsubnet(local.vpc_cidr, 4, k + length(local.azs))]
+  intra_subnets   = [for k, _ in local.azs : cidrsubnet(local.vpc_cidr, 4, k + length(local.azs) * 2)]
 
   enable_nat_gateway = false
 
   enable_dns_hostnames = true
 
+  ### TODO: delete the following after that
   public_subnet_tags = {
     "kind"                                      = "private"
     "kubernetes.io/role/elb"                    = 1
@@ -36,4 +39,74 @@ module "vpc" {
   intra_subnet_tags = {
     "kind" = "intra"
   }
+  ###
+
+  #tags = {
+  #  Environment = "${var.common_prefix}"
+  #}
+}
+
+# ------------------------------------------------------------#
+#  VPC Endpoints
+# ------------------------------------------------------------#
+resource "aws_security_group" "vpc_endpoint" {
+  name   = "${var.prj_prefix}-vpc-endpoint"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    description = "allow all"
+    protocol    = "all"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  #tags = {
+  #  Environment = "${var.prj_prefix}"
+  #}
+}
+
+resource "aws_vpc_endpoint" "endpoints_gateway" {
+  for_each = toset([
+    "s3",
+  ])
+
+  vpc_id          = module.vpc.vpc_id
+  service_name    = "com.amazonaws.${data.aws_region.current.name}.${each.value}"
+  route_table_ids = module.vpc.private_route_table_ids
+
+  #tags = {
+  #  Environment = "${var.prj_prefix}"
+  #}
+}
+
+resource "aws_vpc_endpoint" "endpoints_interface" {
+  for_each = toset([
+    "ecr.api",
+    "ecr.dkr",
+    "logs",
+    "secretsmanager",
+    "ssmmessages",
+  ])
+
+  vpc_id            = module.vpc.vpc_id
+  service_name      = "com.amazonaws.${data.aws_region.current.name}.${each.value}"
+  vpc_endpoint_type = "Interface"
+
+  subnet_ids         = module.vpc.intra_subnets
+  security_group_ids = [aws_security_group.vpc_endpoint.id]
+
+  private_dns_enabled = true
+
+  #tags = {
+  #  Environment = "${var.prj_prefix}"
+  #}
+}
+
+# ------------------------------------------------------------#
+#  Service Discovery
+# ------------------------------------------------------------#
+resource "aws_service_discovery_private_dns_namespace" "dreamkast_development" {
+  name        = "development.local"
+  vpc         = module.vpc.vpc_id
 }
