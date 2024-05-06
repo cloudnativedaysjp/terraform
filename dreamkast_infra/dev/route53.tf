8290031c3faddedfd91322777a1816356dcbbedc
ShotaKitazawa <skitazawa1121@gmail.com>
2024-05-06 11:34:55 +0900

    [dreamkast-dev] switch A record from EKS to ECS

--- dreamkast_infra/dev/route53.tf
+++ dreamkast_infra/dev/route53.tf
@@ -1,11 +1,3 @@
-data "aws_lb" "lb" {
-  tags = {
-    "elbv2.k8s.aws/cluster"    = var.cluster_name
-    "service.k8s.aws/resource" = "LoadBalancer"
-    "service.k8s.aws/stack"    = "projectcontour/envoy"
-  }
-}
-
 data "aws_route53_zone" "cloudnativedays_jp" {
   name = "cloudnativedays.jp"
 }
@@ -16,11 +8,8 @@ resource "aws_route53_record" "wildcard_dev_cloudnativedays_jp" {
   type    = "A"
 
   alias {
-    # TODO: update the following values
-    #name    = aws_lb.alb.dns_name
-    #zone_id = aws_lb.alb.zone_id
-    name                   = data.aws_lb.lb.dns_name
-    zone_id                = data.aws_lb.lb.zone_id
+    name                   = aws_lb.alb.dns_name
+    zone_id                = aws_lb.alb.zone_id
     evaluate_target_health = true
   }
 }
