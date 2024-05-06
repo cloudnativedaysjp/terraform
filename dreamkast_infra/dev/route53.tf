2462588ca36609659370e682265aafe133963772
ShotaKitazawa <skitazawa1121@gmail.com>
2024-05-06 11:36:40 +0900

    Revert "[dreamkast-dev] switch A record from EKS to ECS"
    This reverts commit e7c77787d96cb13b5ae9aee4d1c1b4d7873fadcc.

--- dreamkast_infra/dev/route53.tf
+++ dreamkast_infra/dev/route53.tf
@@ -1,3 +1,11 @@
+data "aws_lb" "lb" {
+  tags = {
+    "elbv2.k8s.aws/cluster"    = var.cluster_name
+    "service.k8s.aws/resource" = "LoadBalancer"
+    "service.k8s.aws/stack"    = "projectcontour/envoy"
+  }
+}
+
 data "aws_route53_zone" "cloudnativedays_jp" {
   name = "cloudnativedays.jp"
 }
@@ -8,8 +16,11 @@ resource "aws_route53_record" "wildcard_dev_cloudnativedays_jp" {
   type    = "A"
 
   alias {
-    name                   = aws_lb.alb.dns_name
-    zone_id                = aws_lb.alb.zone_id
+    # TODO: update the following values
+    #name    = aws_lb.alb.dns_name
+    #zone_id = aws_lb.alb.zone_id
+    name                   = data.aws_lb.lb.dns_name
+    zone_id                = data.aws_lb.lb.zone_id
     evaluate_target_health = true
   }
 }
