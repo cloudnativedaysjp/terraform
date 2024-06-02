df0beaeb77cfacf30551ca0f3b4dff0749c74a80
ShotaKitazawa <skitazawa1121@gmail.com>
2024-06-02 17:55:56 +0900

    [dreamkast-dev] delete EKS

--- dreamkast_infra/dev/terraform.tf
+++ dreamkast_infra/dev/terraform.tf
@@ -22,9 +22,3 @@ provider "aws" {
 data "aws_eks_cluster_auth" "default" {
   name = var.cluster_name
 }
-
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.default.token
-}
