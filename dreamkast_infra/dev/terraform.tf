34755a065ab2f1c926316f53c5fc999104136eef
ShotaKitazawa <skitazawa1121@gmail.com>
2024-06-02 19:01:24 +0900

    [dreamkast-dev] delete EKS

--- dreamkast_infra/dev/terraform.tf
+++ dreamkast_infra/dev/terraform.tf
@@ -17,8 +17,3 @@ terraform {
 provider "aws" {
   region = "us-west-2"
 }
-
-# https://github.com/terraform-aws-modules/terraform-aws-eks/issues/2009
-data "aws_eks_cluster_auth" "default" {
-  name = var.cluster_name
-}
