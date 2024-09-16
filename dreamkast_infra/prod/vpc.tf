7442d6bae99843084278288a9d53a409157f51ff
ShotaKitazawa <skitazawa1121@gmail.com>
2024-09-16 20:33:00 +0900

    add CloudMap SD

--- dreamkast_infra/prod/vpc.tf
+++ dreamkast_infra/prod/vpc.tf
@@ -56,3 +56,18 @@ resource "aws_service_discovery_private_dns_namespace" "dreamkast_production" {
   name = "production.local"
   vpc  = module.vpc.vpc_id
 }
+
+resource "aws_service_discovery_service" "dreamkast_dk" {
+  name = "dreamkast"
+  dns_config {
+    namespace_id = aws_service_discovery_private_dns_namespace.dreamkast_production.id
+    dns_records {
+      ttl  = 10
+      type = "A"
+    }
+    routing_policy = "MULTIVALUE"
+  }
+  health_check_custom_config {
+    failure_threshold = 1
+  }
+}
