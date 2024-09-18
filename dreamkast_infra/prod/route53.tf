9691b8a94460f9011f8c7d4b2352f2ce287a2af8
ShotaKitazawa <skitazawa1121@gmail.com>
2024-09-18 21:59:04 +0900

    switch dreamkast production to ECS environment

--- dreamkast_infra/prod/route53.tf
+++ dreamkast_infra/prod/route53.tf
@@ -1,11 +1,3 @@
-data "aws_lb" "lb" {
-  tags = {
-    "elbv2.k8s.aws/cluster"    = var.cluster_name
-    "service.k8s.aws/resource" = "LoadBalancer"
-    "service.k8s.aws/stack"    = "projectcontour/envoy"
-  }
-}
-
 data "aws_route53_zone" "cloudnativedays_jp" {
   name = "cloudnativedays.jp"
 }
@@ -15,8 +7,8 @@ resource "aws_route53_record" "wildcard_cloudnativedays_jp" {
   name    = "*.cloudnativedays.jp"
   type    = "A"
   alias {
-    name                   = data.aws_lb.lb.dns_name
-    zone_id                = data.aws_lb.lb.zone_id
+    name                   = aws_lb.alb.dns_name
+    zone_id                = aws_lb.alb.zone_id
     evaluate_target_health = true
   }
 }
