2aae2bbbf051a25561ba293c8621c8c2eacc7300
ne-sachirou <ne-sachirou@hatena.ne.jp>
2024-09-26 21:06:54 +0900

    外部ID

--- o11y_infra/prd/mackerel_aws_integration.tf
+++ o11y_infra/prd/mackerel_aws_integration.tf
@@ -9,7 +9,7 @@ data "aws_iam_policy_document" "mackerel_aws_integration_assume_role_policy" {
     condition {
       test     = "StringEquals"
       variable = "sts:ExternalId"
-      values   = ["iUmC8uhM5dYZt9RuSikXDwGWvBlCeOZSezLBKxcr"]
+      values   = ["P6vnw10qZcylI8QWWXOxX1oneICZOrShzBAnFCf6"]
     }
   }
 }
