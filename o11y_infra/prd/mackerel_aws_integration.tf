943718a5e910fb71f4f72ecb6bd92539f70c2418
ne-sachirou <ne-sachirou@hatena.ne.jp>
2024-11-21 20:38:37 +0900

    Revert "MackerelのAWSインテグレーションの設定を書き直し"
    This reverts commit 72be3dc6f8b21b68d2e70884c7e5897c7d89c534.

--- o11y_infra/prd/mackerel_aws_integration.tf
+++ o11y_infra/prd/mackerel_aws_integration.tf
@@ -9,8 +9,26 @@ data "aws_iam_policy_document" "mackerel_aws_integration_assume_role_policy" {
     condition {
       test     = "StringEquals"
       variable = "sts:ExternalId"
-      values   = ["fQeRdQJ1dJbWrUwGDNpunLs5tns5AyXsYJ2niRxR"]
+      values   = ["P6vnw10qZcylI8QWWXOxX1oneICZOrShzBAnFCf6"]
     }
+  }
+}
+
+data "aws_iam_policy_document" "mackerel_aws_integration_inline_policy" {
+  statement {
+    actions   = [
+      "AWSBudgetsReadOnlyAccess",
+      "AmazonEC2ReadOnlyAccess",
+      "AmazonElastiCacheReadOnlyAccess",
+      "AmazonRDSReadOnlyAccess",
+      "AmazonSQSReadOnlyAccess",
+      "CloudWatchReadOnlyAccess",
+      "ecs:Describe*",
+      "ecs:List*",
+      "sqs:ListQueueTags"
+    ]
+    effect    = "Allow"
+    resources = ["*"]
   }
 }
 
@@ -18,58 +36,8 @@ resource "aws_iam_role" "mackerel_aws_integration" {
   name = "mackerel-aws-integration"
   path = "/"
   assume_role_policy = data.aws_iam_policy_document.mackerel_aws_integration_assume_role_policy.json
-}
-
-# ALB インテグレーション
-resource "aws_iam_role_policy_attachment" "mackerel_aws_integration_AmazonEC2ReadOnlyAccess_policy_attachment" {
-  role       = aws_iam_role.mackerel_aws_integration.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"
-}
-
-# ElastiCache インテグレーション
-resource "aws_iam_role_policy_attachment" "mackerel_aws_integration_AmazonElastiCacheReadOnlyAccess_policy_attachment" {
-  role       = aws_iam_role.mackerel_aws_integration.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonElastiCacheReadOnlyAccess"
-}
-
-# RDS インテグレーション
-resource "aws_iam_role_policy_attachment" "mackerel_aws_integration_AmazonRDSReadOnlyAccess_policy_attachment" {
-  role       = aws_iam_role.mackerel_aws_integration.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSReadOnlyAccess"
-}
-
-# SQS インテグレーション
-resource "aws_iam_role_policy_attachment" "mackerel_aws_integration_AmazonSQSReadOnlyAccess_policy_attachment" {
-  role       = aws_iam_role.mackerel_aws_integration.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSQSReadOnlyAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "mackerel_aws_integration_CloudWatchReadOnlyAccess_policy_attachment" {
-  role       = aws_iam_role.mackerel_aws_integration.name
-  policy_arn = "arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess"
-}
-
-data "aws_iam_policy_document" "mackerel_aws_integration_inline_policy" {
-  statement {
-    actions   = [
-      "ecs:Describe*", # ECS インテグレーション
-      "ecs:List*", # ECS インテグレーション
-      "elasticache:ListTagsForResource", # ElastiCache インテグレーション
-      "sqs:ListQueueTags" # SQS インテグレーション
-    ]
-    effect    = "Allow"
-    resources = ["*"]
+  inline_policy {
+    name   = "mackerel-aws-integration"
+    policy = data.aws_iam_policy_document.mackerel_aws_integration_inline_policy.json
   }
-}
-
-resource "aws_iam_policy" "mackerel_aws_integration_inline_policy" {
-  name        = "mackerel-aws-integration-inline-policy"
-  description = "Mackerel AWS Integration Inline Policy"
-  policy      = data.aws_iam_policy_document.mackerel_aws_integration_inline_policy.json
-}
-
-resource "aws_iam_policy_attachment" "mackerel_aws_integration_inline_policy_attachment" {
-  name       = "mackerel-aws-integration-inline-policy-attachment"
-  roles      = [aws_iam_role.mackerel_aws_integration.name]
-  policy_arn = aws_iam_policy.mackerel_aws_integration_inline_policy.arn
 }
