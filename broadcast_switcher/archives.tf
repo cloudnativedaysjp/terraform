c7b9be81db2b79e3521e9ab2cd3d516450f29025
Kazuto Kusama <jacopen@gmail.com>
2024-11-21 19:33:21 +0900

    Update sakura image

--- broadcast_switcher/archives.tf
+++ broadcast_switcher/archives.tf
@@ -1,15 +1,15 @@
 data "sakuracloud_archive" "ubuntu" {
   filter {
-    id = "113301413483"
+    id = "113600510443"
   }
 }
 data "sakuracloud_archive" "ubuntu2204" {
   filter {
-    id = "113401132828"
+    id = "113600510445"
   }
 }
 data "sakuracloud_archive" "windows" {
   filter {
-    id = "113500661751"
+    id = "113602000481"
   }
 }
