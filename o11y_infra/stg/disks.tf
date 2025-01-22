8cb284941f8318e1746b888efd5cbb1f05c74e08
shukawam <shukawam@gmail.com>
2025-01-22 19:47:48 +0900

    Update volume parameter

--- o11y_infra/stg/disks.tf
+++ o11y_infra/stg/disks.tf
@@ -18,7 +18,7 @@ resource "sakuracloud_disk" "o11y_stacks_boot" {
   source_archive_id = data.sakuracloud_archive.ubuntu2404.id
   plan              = "ssd"
   connector         = "virtio"
-  size              = 50
+  size              = 20
 }
 
 resource "sakuracloud_disk" "o11y_stacks_docker_volume" {
