814a4f54c3109c6981d945f2a17e8d94014fc21f
b1gb4by <34154552+b1gb4by@users.noreply.github.com>
2025-02-10 14:59:57 +0000

    update: update sentry spec

--- o11y_infra/prd/disks.tf
+++ o11y_infra/prd/disks.tf
@@ -18,26 +18,6 @@ resource "sakuracloud_disk" "sentry_docker_volume" {
   size      = 500
 }
 
-resource "sakuracloud_disk" "sentry_redis_boot" {
-  name              = "sentry-redis-boot"
-  source_archive_id = data.sakuracloud_archive.ubuntu22042.id
-  plan              = "ssd"
-  connector         = "virtio"
-  size              = 100
-  lifecycle {
-    ignore_changes = [
-      source_archive_id,
-    ]
-  }
-}
-
-resource "sakuracloud_disk" "sentry_redis_docker_volume" {
-  name      = "sentry-redis-docker-volume"
-  plan      = "ssd"
-  connector = "virtio"
-  size      = 250
-}
-
 resource "sakuracloud_disk" "o11y_stacks_boot" {
   name              = "o11y-stacks-prd"
   source_archive_id = data.sakuracloud_archive.ubuntu2404.id
