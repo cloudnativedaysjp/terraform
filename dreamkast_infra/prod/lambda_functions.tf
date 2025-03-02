80c7fd2c100aaf6cee330ef25d618425ef5c78d6
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2025-03-02 12:20:15 +0900

    chore: update retention in days of cloudwatch log groups if it's never expires

--- dreamkast_infra/prod/lambda_functions.tf
+++ dreamkast_infra/prod/lambda_functions.tf
@@ -1,5 +1,5 @@
-module "log_retention_lambda" {
-  source = "../modules/lambda"
+module "log_retention_lambda_ap_northeast_1" {
+  source = "../modules/log_retention_lambda_function"
 
   aws_region = "ap-northeast-1"
 }
