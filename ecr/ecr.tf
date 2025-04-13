cc55864fe59e74ff9b1f23545ef6349e11de1130
ShotaKitazawa <skitazawa1121@gmail.com>
2025-04-13 17:56:43 +0900

    update ECR Lifecycle policy

--- ecr/ecr.tf
+++ ecr/ecr.tf
@@ -40,9 +40,23 @@ resource "aws_ecr_lifecycle_policy" "us_west_2" {
     "rules": [
         {
             "rulePriority": 1,
-            "description": "Expire images older than 30 days",
+            "description": "Expire untagged images older than 3 days",
             "selection": {
                 "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 3
+            },
+            "action": {
+                "type": "expire"
+            }
+        },
+        {
+            "rulePriority": 2,
+            "description": "Expire images older than 30 days",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["commit-"],
                 "countType": "sinceImagePushed",
                 "countUnit": "days",
                 "countNumber": 30
