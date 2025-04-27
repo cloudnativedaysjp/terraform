1ecd3396f9344e11975bdc48fa076be8e9acea34
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2025-04-27 18:48:44 +0900

    feat: extend EventBridge scheduler to stop RDS instances daily at 23:00

--- dreamkast_infra/stg/scheduler.tf
+++ dreamkast_infra/stg/scheduler.tf
@@ -1,8 +1,8 @@
 # ------------------------------------------------------------#
-# EventBridge Scheduler to stop ECS services at 23:00 daily
+# EventBridge Scheduler to stop ECS services and RDS at 23:00 daily
 # ------------------------------------------------------------#
 
-# IAM role for EventBridge to stop ECS services
+# IAM role for EventBridge to stop ECS services and RDS
 resource "aws_iam_role" "eventbridge_stop_ecs_role" {
   name = "${var.prj_prefix}-eventbridge-stop-ecs-role"
 
@@ -41,10 +41,36 @@ resource "aws_iam_policy" "stop_ecs_policy" {
   })
 }
 
-# Attach the policy to the role
+# IAM policy for stopping RDS instances
+resource "aws_iam_policy" "stop_rds_policy" {
+  name        = "${var.prj_prefix}-stop-rds-policy"
+  description = "Policy to allow stopping RDS instances"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "rds:StopDBInstance",
+          "rds:DescribeDBInstances"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Attach the ECS policy to the role
 resource "aws_iam_role_policy_attachment" "stop_ecs_policy_attachment" {
   role       = aws_iam_role.eventbridge_stop_ecs_role.name
   policy_arn = aws_iam_policy.stop_ecs_policy.arn
+}
+
+# Attach the RDS policy to the role
+resource "aws_iam_role_policy_attachment" "stop_rds_policy_attachment" {
+  role       = aws_iam_role.eventbridge_stop_ecs_role.name
+  policy_arn = aws_iam_policy.stop_rds_policy.arn
 }
 
 # EventBridge scheduler to stop dreamkast service at 23:00 daily
@@ -89,6 +115,29 @@ resource "aws_scheduler_schedule" "stop_dreamkast_ui_service" {
       cluster        = aws_ecs_cluster.dreamkast_stg.name
       service        = "${var.prj_prefix}-dreamkast-ui"
       desiredCount   = 0
+    })
+  }
+}
+
+# ------------------------------------------------------------#
+# EventBridge scheduler to stop RDS instance at 23:00 daily
+# ------------------------------------------------------------#
+resource "aws_scheduler_schedule" "stop_rds_instance" {
+  name        = "${var.prj_prefix}-stop-rds-instance"
+  description = "Stop RDS instance in staging environment at 23:00 daily"
+  
+  flexible_time_window {
+    mode = "OFF"
+  }
+  
+  schedule_expression = "cron(0 14 * * ? *)" # 23:00 JST (14:00 UTC)
+  
+  target {
+    arn      = "arn:aws:scheduler:::aws-sdk:rds:stopDBInstance"
+    role_arn = aws_iam_role.eventbridge_stop_ecs_role.arn
+    
+    input = jsonencode({
+      DBInstanceIdentifier = aws_db_instance.rds_instance.identifier
     })
   }
 }
